111ed70e43af127abf93049be4db6985a83228a0
Your Name <you@example.com>
2019-11-27 13:49:14 +0800

    14

--- copy.sh
+++ copy.sh
@@ -19,8 +19,8 @@ for Pvc in "$PVCS"
 do
 	#TODO: COPY TAR to container
 	retry -f "echo 'get pvc fail'; exit 1"  "kubectl get pvc -n $Namespace $Pvc -o json > old-${Pvc}-pvc.json"
-	retry -f "echo 'get pv fail'; exit 1"   "kubectl get pv $Pv -o json > old-${Pvc}-pv.json"
 	Pv=$(cat old-${Pvc}-pvc.json | tr '\r\n' ' '| jq -r '.spec.volumeName')
+	retry -f "echo 'get pv fail'; exit 1"   "kubectl get pv $Pv -o json > old-${Pvc}-pv.json"
 	echo "Pvc:$Pvc"
 	VolumName=$( cat pod.json | tr '\r\n' ' '|   jq -r ".
 	| .spec.volumes[] 
@@ -32,14 +32,26 @@ do
 	| select(.name==\"$VolumName\" ) 
 	| .mountPath")
 	echo "PodPath:$PodPath"
-	HOSTPATH=$(cat old-${Pvc}-pv.json | tr '\r\n' ' '| jq -r ".
+	HostPath=$(cat old-${Pvc}-pv.json | tr '\r\n' ' '| jq -r ".
 	| (.spec.hostPath.path)
 	")
-	echo "ToPath:$RootPath/$HOSTPATH"
+	echo "ToPath:$RootPath/$HostPath"
+	[ -z "$HostPath" ] && echo "HostPath is empty" && exit 1
 	
-	not_exist=[ ! -f $ROOTPATH/$HOSTPATH ]
-	if $not_exist; then
-		mkdir -p $ROOTPATH/$HOSTPATH
+	if [[ -f "$RootPath/$HostPath" ]]; 
+	then FileExist=0
+	else FileExist=1
+	fi;
+	FileExist=$(test -e "$RootPath/$HostPath";echo $?  )
+
+	if [[ $FileExist -eq 0 ]]; 
+	then echo "bakup dir";mv  $RootPath/$HostPath       $RootPath/$(dirname $HostPath)/.$(basename $HostPath)
+	else echo "crate dir";
 	fi
-	retry -f "if $not_exist;then rm -fr $RootPath/$HOSTPATH;fi ;echo 'cp is fail,Maybe tar not in container'; exit 2" "kubectl cp $Namespace/$Pod:$PodPath $ROOTPATH/$HOSTPATH"
+
+	retry -f "if [ $FileExist -eq 0 ];
+	then echo 'recovery';rm -fr $RootPath/$HostPath ; mv $RootPath/$(dirname $HostPath)/.$(basename $HostPath)   $RootPath/$HostPath;
+	else rm -fr $RootPath/$HostPath ; fi; echo 'cp is fail,Maybe tar not in container'; exit 2" "kubectl cp $Namespace/$Pod:$PodPath $RootPath/$HostPath"
+
+	rm -fr $RootPath/$(dirname $HostPath)/.$(basename $HostPath)
 done
